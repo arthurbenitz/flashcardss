cad05c4e627d4d701757c4362b7c871eda37dd11
arthurbenitz <arthurbenitz2007@gmail.com>
2024-11-11 19:35:22 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,61 @@
+criaCartao(
+    'Curiosidade',
+    'Qual é o maior planeta do sistema solar?',
+    'Júpiter.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Quem escreveu Dom Quixote?',
+    'Miguel de Cervantes.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Quantos ossos há no corpo humano adulto?',
+    '206.'
+)
+
+criaCartao(
+    'Curiosidade',
+    'Qual é a capital do Japão?',
+    'Tóquio.'
+)
+
+
+
+
+criaCartao(
+    'Curiosidade',
+    'Qual animal é conhecido por mudar de cor?',
+    'Camaleão.')
+
+
+    criaCartao(
+        'Curiosidade',
+        'Quem pintou a Mona Lisa?',
+        'Leonardo da Vinci.')
+
+
+
+
+        criaCartao(
+            'Curiosidade',
+            'Qual é o metal mais leve?',
+            'Lítio.')
+
+
+
+
+            criaCartao(
+                'Curiosidade',
+                'Quem inventou a lâmpada?',
+                'Thomas Edison.')
+
+
+
+
+                criaCartao(
+                    'Curiosidade',
+                    'Que animal é símbolo da Austrália?',
+                    'Canguru.')
